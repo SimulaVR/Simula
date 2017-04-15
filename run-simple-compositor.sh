25a33cbcfe98941f45c189163c9d14042ba3c82c
David Kraeutmann <kane@kane.cx>
2017-04-15 22:59:32 +0200

    Fix script

--- run-simple-compositor.sh
+++ run-simple-compositor.sh
@@ -1,1 +1,2 @@
-LD_LIBRARY_PATH=/home/kane/simula/SimulaHS/simula_cpp/lib:/home/kane/simula/SimulaHS/simula_cpp/dependencies/build/qt5/lib/ /home/kane/simula/SimulaHS/.stack-work/install/x86_64-linux/lts-8.9/8.0.2/bin/simple-compositor
+ROOTDIR=$(dirname $0)
+LD_LIBRARY_PATH=$ROOTDIR/simula_cpp/lib:$ROOTDIR/simula_cpp/dependencies/build/qt5/lib/ $ROOTDIR/.stack-work/install/x86_64-linux/lts-8.9/8.0.2/bin/simple-compositor
