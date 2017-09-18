90b550a7cd906df55c4e7bf7d3c14593dfdb3a77
George Singer <george.w.singer@gmail.com>
2017-09-18 12:36:16 -0400

    Changed permission of swrast.sh.

--- shell.nix
+++ shell.nix
@@ -11,7 +11,7 @@ pkgs.haskell.lib.buildStackProject {
                              dbus
                              (callPackage simula-wayland/weston2.nix { })
                              (callPackage simula-osvr/OSVR-Core.nix { })
-                             (callPackage simula-osvr/OSVR-Vive.nix { })
+                             # (callPackage simula-osvr/OSVR-Vive.nix { })
                              libxml2
                              libxkbcommon
                           ];
