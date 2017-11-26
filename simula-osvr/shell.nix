bda0b83fcb819fbfcb5b32db31c33ae073554ff7
Zac Slade <krakrjak@gmail.com>
2017-11-26 00:17:59 -0600

    OSVR is out. Build tools are in.

--- simula-osvr/shell.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import <nixpkgs> {}, ghc ? pkgs.ghc }:
-
-pkgs.haskell.lib.buildStackProject {
-  name = "simula-osvr";
-  inherit ghc;
-  buildInputs = with pkgs; [(callPackage ./OSVR-Core.nix { })]; # provides osvrClientKit
-  LANG = "en_US.UTF-8";
-  TMPDIR = "/tmp";
-}
