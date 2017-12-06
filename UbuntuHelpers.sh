45c745376bbc057b0b3303b61e5f2255da85f3d1
George Singer <george.w.singer@gmail.com>
2017-12-06 09:08:30 -0500

    Updated UbuntuHelpers.sh

--- UbuntuHelpers.sh
+++ UbuntuHelpers.sh
@@ -41,7 +41,7 @@ installUbuntuDependencies() {
       libusb-1.0-0-dev \
       libspdlog-dev \
       libeigen3-dev \
-      libvulkan-dev
+      libvulkan-dev \
 }
 
 installWaylandProtocols() {
@@ -54,15 +54,30 @@ installWaylandProtocols() {
   sudo make install
 }
 
-installWeston2() {
+# requires Wayland Protocols
+installWeston() {
   cd /tmp
   git clone https://github.com/wayland-project/weston.git weston
   cd /tmp/weston
-  git checkout -b v2.0.0 2.0.0
+  if [ -z $1 ]; then
+    git checkout -b v3.0.0 3.0.0
+  else
+    git checkout -b v"$1" "$1" # allows us to opt in to alternative versions i.e. "2.0.0"
+  fi
   ./autogen.sh
   ./configure
   make
   sudo make install
+}
+
+update_gcc_to_7.2() {
+  sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+  sudo apt-get update
+  sudo apt-get install gcc-7
+  sudo apt-get install g++-7
+  sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 60 --slave /usr/bin/g++ g++ /usr/bin/g++-7
+  # `gcc -v` should yield version 7.2
+  # `g++ -v` should yield version 7.2
 }
 
 installSteam() {
@@ -97,10 +112,26 @@ installOpenVR() {
     #                /usr/local/lib/libopenvr_api.so
 }
 
+# If you have Ubuntu 17.10 you can just run `sudo apt-get install steam-devices` instead of this command
 addViveUdevRules() {
-  # This package automatically adds Vive udev rules to
-  # /lib/udev/rules.d/60-HTC-Vive-perms.rules
-  sudo apt-get install steam-devices
+  local VIVE_RULES="/lib/udev/rules.d/60-HTC-Vive-perms.rules";
+
+  if [ ! -f /lib/udev/rules.d/60-HTC-Vive-perms.rules ]; then
+    echo '# HTC Vive HID Sensor naming and permissioning'                                                            >> $VIVE_RULES
+    echo 'KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="0bb4", ATTRS{idProduct}=="2c87", TAG+="uaccess"' >> $VIVE_RULES
+    echo 'KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2101", TAG+="uaccess"' >> $VIVE_RULES
+    echo 'KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2000", TAG+="uaccess"' >> $VIVE_RULES
+    echo 'KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="1043", TAG+="uaccess"' >> $VIVE_RULES
+    echo 'KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2050", TAG+="uaccess"' >> $VIVE_RULES
+    echo 'KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2011", TAG+="uaccess"' >> $VIVE_RULES
+    echo 'KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2012", TAG+="uaccess"' >> $VIVE_RULES
+    echo 'SUBSYSTEM=="usb", ATTRS{idVendor}=="0bb4", ATTRS{idProduct}=="2c87", TAG+="uaccess"'                       >> $VIVE_RULES
+    echo '# HTC Camera USB Node'                                                                                     >> $VIVE_RULES
+    echo 'SUBSYSTEM=="usb", ATTRS{idVendor}=="114d", ATTRS{idProduct}=="8328", TAG+="uaccess"'                       >> $VIVE_RULES
+    echo '# HTC Mass Storage Node'                                                                                   >> $VIVE_RULES
+    echo 'SUBSYSTEM=="usb", ATTRS{idVendor}=="114d", ATTRS{idProduct}=="8200", TAG+="uaccess"'                       >> $VIVE_RULES
+    echo 'SUBSYSTEM=="usb", ATTRS{idVendor}=="114d", ATTRS{idProduct}=="8a12", TAG+="uaccess"'                       >> $VIVE_RULES
+  fi
 }
 
 # It is best to install stack via curl to avoid
@@ -113,24 +144,27 @@ installStack() {
 # Use this if you already have stack installed via apt-get install.
 upgradeStack() {
   cd ~
-  stack upgrade                                          # Old versions of stack can cause Simula build trouble
-  stack install hpack                                    # Same with old versions of hpack
-  echo 'export PATH=$HOME/.local/bin:$PATH' >> ~/.bashrc # Ensures stack programs like hpack are on your PATH
-}
-
-# Here we emphasize that Simula should be cloned recursively to
-# bring in all necessary submodules.
-buildSimulaInHomeDirectory() {
-  cd ~
-  git clone --recursive https://github.com/SimulaVR/Simula Simula
-  cd ~/Simula
   stack setup
-  stack build
+  stack upgrade --git                                    # Old versions of stack can cause Simula build trouble
+  stack install hpack                                    # Same with old versions of hpack
 }
 
-# Here we emphasize that changing branches should result in
-# a submodule update.
-changeBranches() {
-    git checkout "$1"
-    git submodule update --init --recursive
+buildSimulaDependencies() {
+  installUbuntuDependencies
+  installWaylandProtocols
+  installWeston
+  update_gcc_to_7.2
+  installSteam
+  installNvidiaDrivers
+  installOpenVR
+  addViveUdevRules
+  installStack
+  upgradeStack
+}
+
+buildSimula() {
+  make init
+  stack setup --install-ghc
+  stack build
+  echo "Remember to open steam and install and run SteamVR before launching Simula."
 }
