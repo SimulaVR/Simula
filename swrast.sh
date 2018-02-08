032b867ff76e44d5ac27a2bb810c3d0601a70dda
Ludvig Böklin <ludvig.boklin@protonmail.com>
2018-02-08 20:29:25 +0100

    Fix missing swrast (#32)

--- swrast.sh
+++ swrast.sh
@@ -1,10 +1,11 @@
 # Hack to fix error:
 #    > libEGL warning: DRI2: failed to open swrast (search paths /run/opengl-driver/lib/dri)
 
+# Fix was found here https://github.com/NixOS/nixpkgs/issues/9415#issuecomment-336494579
+
 if [ ! -d /run/opengl-driver ]; then
-    sudo mkdir -p /run
-    sudo ln -s /nix/store/*-mesa-noglu-*-drivers /run/opengl-driver
+    sudo nix-build -A 'mesa_noglu.drivers' $HOME/.nix-defexpr/channels/nixpkgs -o /run/opengl && \
+        sudo mv /run/opengl-drivers /run/opengl-driver
 fi
 
 # TODO: Embed this into shell.nix or stack post-build hook
-# TODO: In case of more than one `/nix/store/*-mesa-noglu-drivers*`, symlink the correct one.
