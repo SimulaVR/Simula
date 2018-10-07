230ae35aa180fce963cf601555193e1446e91165
Ludvig Böklin <ludvig.boklin@protonmail.com>
2018-10-07 11:58:01 +0200

    Don't disable HDR since we are using a patched openvr plugin

--- addons/godot-haskell-plugin/demo/Demo.gd
+++ addons/godot-haskell-plugin/demo/Demo.gd
@@ -9,6 +9,4 @@ func _ready():
     var interface = ARVRServer.find_interface("OpenVR")
     if interface and interface.initialize():
         get_viewport().arvr = true
-        get_viewport().hdr = false
-        get_viewport().rgba8_out = true
 
