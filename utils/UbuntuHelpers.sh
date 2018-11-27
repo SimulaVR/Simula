3e2df76fba3477af3bc46b7ac268dcb8129c5f48
Ludvig Böklin <ludvig.boklin@protonmail.com>
2018-11-27 16:02:26 +0100

    Improve build script

--- utils/UbuntuHelpers.sh
+++ utils/UbuntuHelpers.sh
@@ -1,88 +1,142 @@
 installUbuntuDependencies() {
-   sudo apt install     \
-       libpixman-1-dev  \
-       libweston-3-dev  \
-       libegl1-mesa-dev \
-       weston           \
-       epiphany-browser \
-       curl
+    ask "Install system dependencies? (requires sudo)" Y \
+        && sudo apt install     \
+                libpixman-1-dev  \
+                libweston-3-dev  \
+                libegl1-mesa-dev \
+                weston           \
+                curl
 }
 
 installSteam() {
-     if [ -z `which steam` ]; then
-         sudo add-apt-repository multiverse
-         sudo apt update
-         sudo apt install steam # should include udev rules by default (https://github.com/ValveSoftware/SteamVR-for-Linux#usb-device-requirements)
-     fi
+    if [ -z `which steam` ]; then
+        if [ ask "Steam not installed. Do you consent to enabling the non-free multiverse repo?" ]; then
+            sudo add-apt-repository multiverse \
+                && sudo apt update
+            # Steam should include udev rules by default (https://github.com/ValveSoftware/SteamVR-for-Linux#usb-device-requirements)
+            sudo apt install steam \
+                && sudo udevadm control --reload-rules \
+                && sudo udevadm trigger
+        else echo "Installation of Steam aborted"
+        fi
+    fi
 }
  
-installNvidiaDrivers() {
-     # See https://github.com/ValveSoftware/SteamVR-for-Linux
-     if [ -z `ls /etc/apt/sources.list.d/graphics-drivers-ubuntu-ppa-*.list` ]; then
-         sudo add-apt-repository ppa:graphics-drivers/ppa
-         sudo apt-get update
-     fi
-     sudo apt-get install nvidia-driver-396
-
-     #  The following worked for me against "unmet dependency" errors:
-     #    sudo apt install aptitude
-     #    sudo aptitude install nvidia-driver-396
-}
-
-installAMDDrivers() {
-  # ensure that mesa-17.3+ is installed.
-  echo "TODO."
-}
-
 # Assumes we are in project root
 installGodot() {
   VERSION="3.1"
-  RELEASE="alpha2"
+  RELEASE_TYPE="alpha"
+  RELEASE=${RELEASE_TYPE}"2"
   FILE="Godot_v"${VERSION}"-"${RELEASE}"_x11.64"
   OUT=${PWD}/godot
+  # If it's a stable release, remove the ${RELEASE} part of the URL below
+  URL=https://downloads.tuxfamily.org/godotengine/${VERSION}/${RELEASE}/${FILE}.zip
 
   cd /tmp
-  # If it's a stable release, remove the ${RELEASE} part of the URL below
-  if [ ! -f ${FILE} ]
-  then wget https://downloads.tuxfamily.org/godotengine/${VERSION}/${RELEASE}/${FILE}.zip
+
+  if [ ! `${OUT} --version` = "${VERSION}.${RELEASE_TYPE}.official" ]; then
+      if [ ! -f ${FILE} ] ; then
+          wget ${URL}
+      fi
+      if [ -z `which unzip` ]; then
+          if [ ask "The unzip tool is not installed. Install?" Y ]; then
+              sudo apt install -y unzip
+          else
+              echo "Well.. in that case I can't install Godot."
+              return 1
+          fi
+      fi
+      unzip ${FILE}.zip
+      cp /tmp/${FILE} ${OUT} 
   fi
-  unzip ${FILE}.zip || echo "Could not unzip. Make sure unzip is installed."
-  cp /tmp/${FILE} ${OUT} && echo "\
-Installed Godot (version $(exec ${OUT} --version)): ${OUT}
-Please put it somewhere in your \$PATH"
-  cd -
+
+  if [ -f ${OUT} ]; then
+      echo "Godot (version $(exec ${OUT} --version)) found at: ${OUT}"
+  else
+      echo "${OUT} does not exist."
+      return 1
+  fi
+
+  cd - 1>/dev/null
 }
 
 # It is best to install stack via curl to avoid
 # old versions found in Ubuntu's package stores.
 installStack() {
     if [ -z `which stack` ]; then
-      curl -sSL https://get.haskellstack.org/ | sh -s - -f
-      echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc # ensures stack programs like hpack are on your PATH
-      . ~/.bashrc
-    else 
-      echo "Stack already installed."
+        echo "Installing Stack"
+        curl -sSL https://get.haskellstack.org/ | sh -s - -f
+    else echo "Stack already installed"
     fi
 }
 
 # Use this if you already have stack installed via apt-get install.
 upgradeStack() {
-  #cd ~
   stack upgrade
-  stack setup
-  stack install hpack # Same with old versions of hpack
+  echo ""
+  # Same with old versions of hpack
+  echo "Installing hpack"
+  stack install hpack
 }
 
 buildSimulaDependencies() {
-  installUbuntuDependencies 
-  installSteam
-  # installNvidiaDrivers
-  installStack
-  upgradeStack
+    UBUNTU=`grep -i ubuntu /etc/lsb-release`
+    if [ ! -z ${UBUNTU} ]; then
+        installUbuntuDependencies
+    fi
+
+    installSteam
+    echo ""
+    installGodot
+    echo ""
+    installStack \
+        && echo "" \
+        && upgradeStack
+    echo ""
 }
 
 buildSimula() {
-   buildSimulaDependencies
-   read -e -p "Press any key when SteamVR is launched."
-   make run
+    buildSimulaDependencies \
+        && make
+    echo "
+To run Simula in VR you must make sure to configure your system according
+to the instructions at 'https://github.com/ValveSoftware/SteamVR-for-Linux',
+then use 'make run' while SteamVR is open."
+}
+
+ask() {
+    # https://gist.github.com/davejamesmiller/1965569
+    local prompt default reply
+
+    if [ "${2:-}" = "Y" ]; then
+        prompt="Y/n"
+        default=Y
+    elif [ "${2:-}" = "N" ]; then
+        prompt="y/N"
+        default=N
+    else
+        prompt="y/n"
+        default=
+    fi
+
+    while true; do
+
+        # Ask the question (not using "read -p" as it uses stderr not stdout)
+        echo -n "$1 [$prompt] "
+
+        # Read the answer (use /dev/tty in case stdin is redirected from somewhere else)
+        read reply </dev/tty
+
+        # Default?
+        if [ -z "$reply" ]; then
+            reply=$default
+        fi
+
+        # Check if the reply is valid
+        case "$reply" in
+            Y*|y*) return 0 ;;
+            N*|n*) return 1 ;;
+        esac
+
+    done
 }
