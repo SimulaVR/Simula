310390980a698241fc2519de6aaec373afd67798
Ludvig Böklin <ludvig.boklin@protonmail.com>
2018-12-11 21:47:15 +0100

    Parameterize VR backend (OpenVR/OpenHMD)
    OpenHMD requires single-threaded mode while compositor needs multi-threading,
    so this is not in a working state.

--- addons/godot-haskell-plugin/src/Plugin/Simula.hs
+++ addons/godot-haskell-plugin/src/Plugin/Simula.hs
@@ -1,23 +1,18 @@
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE TypeFamilies #-}
 module Plugin.Simula (GodotSimula(..)) where
 
 import           Plugin.Imports
 
-import           Control.Exception
-
-import           Foreign ( deRefStablePtr
-                         , castPtrToStablePtr
-                         )
-
 import           Plugin.Input
 import           Plugin.Input.Grab
 import           Plugin.SimulaController
 import           Plugin.WestonSurfaceSprite
+import           Plugin.VR
 
 import           Godot.Core.GodotGlobalConstants
 import           Godot.Extra.Register
 import           Godot.Nativescript
-import qualified Godot.Gdnative.Internal.Api as Api
 import qualified Godot.Methods as G
 
 
@@ -45,99 +40,29 @@ instance HasBaseClass GodotSimula where
   super (GodotSimula obj _) = GodotNode obj
 
 
-data VRInitResult
-  = InitVRSuccess
-  | InitVRFailed
-
-
 ready :: GFunc GodotSimula
 ready self _ = do
-  gwc <- "res://addons/godot-haskell-plugin/WestonCompositor.gdns"
-    & unsafeNewNS GodotSpatial "Spatial" []
-  G.set_name gwc =<< toLowLevel "Weston"
-  G.add_child self (asObj gwc) True
+  addCompositorNode
 
-  initVR (safeCast self) >>= \case
-    InitVRSuccess -> do
-      p <- unsafeInstance GodotARVROrigin "ARVROrigin"
-      G.add_child self (asObj p) True
-
-      hmd <- unsafeInstance GodotARVRCamera "ARVRCamera"
-      G.add_child self (asObj hmd) True
-
-      newController p "LeftController"
-        >>= (`G.set_controller_id` 1)
-      newController p "RightController"
-        >>= (`G.set_controller_id` 2)
-
-      return ()
-
-    InitVRFailed -> return ()
+  -- OpenHMD is unfortunately not yet a working substitute for OpenVR
+  -- https://github.com/SimulaVR/Simula/issues/72
+  {-
+   -backend <- do
+   -  !cfg <- openHMDConfig
+   -  return $ OpenHMD cfg
+   -}
+  let backend = OpenVR
+  initVR backend (safeCast self) >>= \case
+    InitVRSuccess _ -> addVRNodes (safeCast self) backend
+    InitVRFailed    -> return ()
 
   retnil
-  where
-    connectController :: GodotSimulaController -> IO ()
-    connectController ct = do
-      btnPressed <- toLowLevel "button_pressed"
-      btnReleased <- toLowLevel "button_release"
-      btnSignal <- toLowLevel "on_button_signal"
-
-      argsPressed <- Api.godot_array_new
-      toLowLevel (toVariant $ asObj ct) >>= Api.godot_array_append  argsPressed
-      toLowLevel (toVariant True) >>= Api.godot_array_append argsPressed
-
-      argsReleased <- Api.godot_array_new
-      toLowLevel (toVariant $ asObj ct) >>= Api.godot_array_append  argsReleased
-      toLowLevel (toVariant False) >>= Api.godot_array_append argsReleased
-
-      G.connect ct btnPressed (asObj self) btnSignal argsPressed 0
-      G.connect ct btnReleased (safeCast self) btnSignal argsReleased 0
-
-      return ()
-
-    newController :: GodotARVROrigin -> Text -> IO GodotSimulaController
-    newController p name = do
-      ct <- "res://addons/godot-haskell-plugin/SimulaController.gdns"
-        & unsafeNewNS id "Object" []
-        -- TODO: Make this implicit in newNS (godot-extra)?
-        >>= Api.godot_nativescript_get_userdata
-        >>= deRefStablePtr . castPtrToStablePtr
-      G.add_child p (asObj ct) True
-      G.set_name ct =<< toLowLevel name
-      connectController ct
-
-      return ct
-
-
-initVR :: GodotNode -> IO VRInitResult
-initVR node =
-  handle onVariantError $
-    findInterface
-      >>= G.initialize
-      >>= \case
-        True  -> initSuccess
-        False -> initFailed
  where
-  initSuccess :: IO VRInitResult
-  initSuccess = do
-    G.get_viewport node >>= (`G.set_use_arvr` True)
-    godotPrint "Initialized OpenVR"
-    return InitVRSuccess
-
-  initFailed :: IO VRInitResult
-  initFailed = do
-    godotPrint "Failed to initialize OpenVR"
-    return InitVRFailed
-
-  onVariantError :: GodotVariantCallError -> IO VRInitResult
-  onVariantError _ = do
-    godotPrint "Failed to load OpenVR because of Variant shenanigans."
-    return InitVRFailed
-
-  findInterface :: IO GodotARVRInterface
-  findInterface = do
-    arvrServer <- getSingleton GodotARVRServer "ARVRServer"
-    G.find_interface arvrServer =<< toLowLevel "OpenVR"
+  addCompositorNode = do
+    gwc <- "res://addons/godot-haskell-plugin/WestonCompositor.gdns"
+      & unsafeNewNS GodotSpatial "Spatial" []
+    G.set_name gwc =<< toLowLevel "Weston"
+    G.add_child self (asObj gwc) True
 
 
 on_button_signal :: GFunc GodotSimula
