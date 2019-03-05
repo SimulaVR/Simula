3e897d59d88cb00c3bc1df53feba6874200401bc
George Singer <george.w.singer@gmail.com>
2019-03-05 18:41:34 -0500

    Drafted wlr_godot_backend_create w/a new backend_get_renderer.
    Simula runs but xdg apps don't launch.

--- addons/godot-haskell-plugin/include/wlr_godot_backend.h
+++ addons/godot-haskell-plugin/include/wlr_godot_backend.h
@@ -2,6 +2,18 @@
 #ifndef WLR_GODOT_BACKEND_H
 #define WLR_GODOT_BACKEND_H
 
+
+struct wlr_godot_backend {
+	struct wlr_backend backend;
+	/* struct wlr_egl egl; */
+	struct wlr_renderer *renderer;
+	/* struct wl_display *display; */
+	/* struct wl_list outputs; */
+	/* struct wl_list input_devices; */
+	/* struct wl_listener display_destroy; */
+	/* bool started; */
+};
+
 struct wlr_backend *wlr_godot_backend_create(struct wl_display *display,
                                              wlr_renderer_create_func_t create_renderer_func);
 
