2a0576f25678dee77d4f7a58414b02a77da03587
George Singer <george.w.singer@gmail.com>
2019-04-10 13:47:23 -0400

    Implemented (==) for SimulaView and addWlrChildren.

--- addons/godot-haskell-plugin/src/Plugin/SimulaServer.hs
+++ addons/godot-haskell-plugin/src/Plugin/SimulaServer.hs
@@ -110,9 +110,58 @@ instance HasBaseClass GodotSimulaServer where
 
 ready :: GFunc GodotSimulaServer
 ready gss _ = do
+  addWlrChildren gss
   -- startSimulaServerOnNewThread gss
   -- startTelemetry (gss ^. gssViews) -- Doesn't seem to require previous function to get to displayRunServer
   toLowLevel VariantNil
+
+addWlrChildren :: GodotSimulaServer -> IO ()
+addWlrChildren gss = do
+  -- Here we assume gss is already a node in our scene tree.
+  waylandDisplay <- unsafeInstance GodotWaylandDisplay "WaylandDisplay"
+  G.set_name waylandDisplay =<< toLowLevel "WaylandDisplay"
+  G.add_child gss ((safeCast waylandDisplay) :: GodotObject) True
+  -- We omit having ViewportBounds children
+
+  -- Children of WaylandDisplay
+  wlrDataDeviceManager <- unsafeInstance GodotWlrDataDeviceManager "WlrDataDeviceManager"
+  G.set_name wlrDataDeviceManager =<< toLowLevel "WlrDataDeviceManager"
+  G.add_child waylandDisplay ((safeCast wlrDataDeviceManager) :: GodotObject) True
+
+  wlrBackend <- unsafeInstance GodotWlrBackend "WlrBackend"
+  G.set_name wlrBackend =<< toLowLevel "WlrBackend"
+  G.add_child waylandDisplay ((safeCast wlrBackend) :: GodotObject) True
+
+  wlrXdgShell <- unsafeInstance GodotWlrXdgShell "WlrXdgShell"
+  G.set_name wlrXdgShell =<< toLowLevel "WlrXdgShell"
+  G.add_child waylandDisplay ((safeCast wlrXdgShell) :: GodotObject) True
+
+  wlrSeat <- unsafeInstance GodotWlrSeat "WlrSeat"
+  G.set_name wlrSeat =<< toLowLevel "WlrSeat"
+  G.add_child waylandDisplay ((safeCast wlrSeat) :: GodotObject) True
+
+  wlrKeyboard <- unsafeInstance GodotWlrKeyboard "WlrKeyboard"
+  G.set_name wlrKeyboard =<< toLowLevel "WlrKeyboard"
+  G.add_child waylandDisplay ((safeCast wlrKeyboard) :: GodotObject) True
+
+  -- Children of WlrBackend
+  wlrOutput <- unsafeInstance GodotWlrOutput "WlrOutput"
+  G.set_name wlrOutput =<< toLowLevel "WlrOutput"
+  G.add_child wlrBackend ((safeCast wlrOutput) :: GodotObject) True
+
+  wlrCompositor <- unsafeInstance GodotWlrCompositor "WlrCompositor"
+  G.set_name wlrCompositor =<< toLowLevel "WlrCompositor"
+  G.add_child wlrBackend ((safeCast wlrCompositor) :: GodotObject) True
+
+  -- Update GodotSimulaServer's TVars
+  atomically $ writeTVar (_gssWaylandDisplay gss) waylandDisplay
+  atomically $ writeTVar (_gssWlrBackend gss) wlrBackend
+  atomically $ writeTVar (_gssWlrOutput gss) wlrOutput
+  atomically $ writeTVar (_gssWlrCompositor gss) wlrCompositor
+  atomically $ writeTVar (_gssWlrXdgShell gss) wlrXdgShell
+  atomically $ writeTVar (_gssWlrSeat gss) wlrSeat
+  atomically $ writeTVar (_gssWlrDataDeviceManager gss) wlrDataDeviceManager
+  atomically $ writeTVar (_gssWlrKeyboard gss) wlrKeyboard
 
 -- TODO: check the origin plane?
 moveToUnoccupied :: GodotSimulaServer -> GodotSimulaViewSprite -> IO ()
@@ -157,3 +206,10 @@ initGodotSimulaServer obj = mdo
   -- }
   -- return gss
   return undefined
+
+getSimulaServerNodeFromPath :: GodotSimulaServer -> String -> IO a
+getSimulaServerNodeFromPath gss nodePathStr = do
+  nodePath <- (toLowLevel (pack nodePathStr))
+  gssNode <- G.get_node ((safeCast gss) :: GodotNode) nodePath
+  ret  <- (fromNativeScript (safeCast gssNode)) :: IO a
+  return ret
