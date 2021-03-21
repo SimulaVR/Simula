f670028e4da0359de52f3fba3b713b1b11823547
George Singer <george.w.singer@gmail.com>
2021-03-21 16:15:06 -0500

    Don't suppress GetNixGL.sh stderr

--- utils/GetNixGL.sh
+++ utils/GetNixGL.sh
@@ -12,13 +12,13 @@ NVIDIA_VERSION=$(getNvidiaVersion)
 
 installNixGL() {
     cd ${SIMULA_UTIL_ROOT}/..
-    NIXPKGS_ALLOW_UNFREE=1 nix-env -f ./submodules/godot/nixGLDefault.nix -iA nixGLIntel 2>&1 >/dev/null
+    NIXPKGS_ALLOW_UNFREE=1 nix-env -f ./submodules/godot/nixGLDefault.nix -iA nixGLIntel >/dev/null
     cd -
 }
 
 installNixVulkanNvidia() {
     cd ${SIMULA_UTIL_ROOT}/..
-    NIXPKGS_ALLOW_UNFREE=1 nix-env -f ./submodules/godot/nixGLDefault.nix -iA nixVulkanNvidia 2>&1 >/dev/null
+    NIXPKGS_ALLOW_UNFREE=1 nix-env -f ./submodules/godot/nixGLDefault.nix -iA nixVulkanNvidia >/dev/null
     cd -
 }
 
@@ -26,10 +26,10 @@ if [ -e /etc/NIXOS ]; then
     echo ""
 elif [ -z $NVIDIA_VERSION ]; then
     . $HOME/.nix-profile/etc/profile.d/nix.sh
-    installNixGL 2>&1 >/dev/null;
+    installNixGL >/dev/null;
     which nixGLIntel
 else
     . $HOME/.nix-profile/etc/profile.d/nix.sh
-    installNixVulkanNvidia 2>&1 >/dev/null;
+    installNixVulkanNvidia >/dev/null;
     which nixVulkanNvidia
 fi
