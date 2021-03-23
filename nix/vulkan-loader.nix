4f648bfe35d7d2e55ec20bb7077630a105a51f6c
David Kraeutmann <kane@kane.cx>
2021-03-23 04:31:37 +0100

    Bump custom vulkan loader version

--- nix/vulkan-loader.nix
+++ nix/vulkan-loader.nix
@@ -3,13 +3,13 @@
 
 stdenv.mkDerivation rec {
   pname = "vulkan-loader";
-  version = "1.2.131.2";
+  version = "1.2.162.0";
 
   src = fetchFromGitHub {
     owner = "KhronosGroup";
     repo = "Vulkan-Loader";
     rev = "sdk-${version}";
-    sha256 = "12n4mxc6db89258k8i47ql1zna7k94lkwv7lpxg39nm8ypa1ywrv";
+    sha256 = "0w9i2pliw4ccmjyfzff4i2f3hxwsfd54jg7ahv2v634qmx59bsbi";
   };
 
   nativeBuildInputs = [ pkgconfig ];
