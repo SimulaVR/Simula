7b88f74eb83e67712309fdf39e057b9e1d48faaf
David Kraeutmann <kane@kane.cx>
2021-03-23 04:02:05 +0100

    Update pinned-nixpkgs.nix

--- pinned-nixpkgs.nix
+++ pinned-nixpkgs.nix
@@ -1,6 +1,6 @@
 import (builtins.fetchGit {
   url = "https://github.com/KaneTW/nixpkgs.git";
-  rev = "aebd53b6c5f62c1c88a49c9d5efb5fe88689d763";
+  rev = "bc7d101e7ec14b97f81cca0e88cc21f951ad56de";
   ref = "refs/heads/nixpkgs-unstable";
   name = "nixpkgs-unstable-custom-2021-03-09";
 }) 
