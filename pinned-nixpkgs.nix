f8d94209b901f8e0635d20273c1a0494f540bcc2
George Singer <george.w.singer@gmail.com>
2021-03-24 23:05:06 -0500

    Change pinned-nixpkgs to avoid long compilations

--- pinned-nixpkgs.nix
+++ pinned-nixpkgs.nix
@@ -1,7 +1,8 @@
 import (builtins.fetchGit {
   url = "https://github.com/KaneTW/nixpkgs.git";
-  rev = "bc7d101e7ec14b97f81cca0e88cc21f951ad56de";
+  # rev = "bc7d101e7ec14b97f81cca0e88cc21f951ad56de"; # needed for NUC
+  rev = "5df05c902cde398e056eb6271d5fe13e418db4c6";
   ref = "refs/heads/nixpkgs-unstable";
   name = "nixpkgs-unstable-custom-2021-03-09";
-}) 
+})
 
