ea930e71fecaa7e8e66cb891dd8c6691c4bdc131
George Singer <george.w.singer@gmail.com>
2021-06-08 22:20:14 -0500

    Use SimulaVR/nixpkgs

--- pinned-nixpkgs.nix
+++ pinned-nixpkgs.nix
@@ -1,5 +1,5 @@
 import (builtins.fetchGit {
-  url = "https://github.com/KaneTW/nixpkgs.git";
+  url = "https://github.com/SimulaVR/nixpkgs.git";
   # rev = "bc7d101e7ec14b97f81cca0e88cc21f951ad56de"; # needed for NUC
   rev = "7110179c41e0a866b05187efcbfdac5035d5fb03";
   ref = "refs/heads/fpic";
