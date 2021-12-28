c60e9c193b0d87dc0883c193a8129aabfaed0550
David Kraeutmann <kane@kane.cx>
2021-12-28 08:49:30 +0100

    More stdenv.lib fixes

--- addons/godot-haskell-plugin/godot-haskell-plugin.nix
+++ addons/godot-haskell-plugin/godot-haskell-plugin.nix
@@ -3,6 +3,7 @@
 , http-client-tls, http-types, inline-c, iso8601-time, lens, linear
 , process, raw-strings-qq, stdenv, stm, text, time, unix, uuid
 , vector, fetchFromGitHub, haskellPackages, devBuild ? false, onNixOS ? false, godot, godot-haskell, ordered-containers, process-extras, dhall, hspec, QuickCheck, profileBuild ? false, safe-exceptions, io-streams
+, lib
 }:
 mkDerivation {
   pname = "godot-haskell-plugin";
@@ -15,7 +16,7 @@ mkDerivation {
     stm text time unix uuid vector godot-haskell ordered-containers process-extras dhall hspec QuickCheck safe-exceptions io-streams
   ];
   homepage = "https://github.com/SimulaVR/Simula#readme";
-  license = stdenv.lib.licenses.asl20;
+  license = lib.licenses.asl20;
 
   doHaddock = false;
   doCheck = false;
