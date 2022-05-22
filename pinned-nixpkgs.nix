8ce017f1949b14acf647dd486c3c04199f7d803c
David Kraeutmann <kane@kane.cx>
2022-05-22 03:25:42 +0200

    Bump nixpkgs

--- pinned-nixpkgs.nix
+++ pinned-nixpkgs.nix
@@ -1,6 +1,6 @@
 import (builtins.fetchGit {
   url = "https://github.com/SimulaVR/nixpkgs.git";
-  rev = "d3d0c5d418b17f2f9566f319e537354d0adfee47"; # needed for NUC
+  rev = "acffaa439d445bf55faa1e2b5de3cadbdc9fbda7"; # needed for NUC
   # rev = "7110179c41e0a866b05187efcbfdac5035d5fb03";
   ref = "refs/heads/nuc";
   name = "nixpkgs-unstable-custom-nuc";
