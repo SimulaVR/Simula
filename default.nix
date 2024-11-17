e57b389d671aa10e91cf979f88708855084f1207
haruki7049 <tontonkirikiri@gmail.com>
2024-11-17 10:05:09 +0900

    Delete profileBuild in default.nix

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
-{ onNixOS, devBuild, profileBuild ? false }:
+{ onNixOS, devBuild }:
 let
-    pkgs = if profileBuild then (import ./pinned-nixpkgs.nix { overlays = (import ./nix/profileOverlays.nix); }) else (import ./pinned-nixpkgs.nix { });
+    pkgs = import ./pinned-nixpkgs.nix { };
 in
-pkgs.callPackage ./Simula.nix { onNixOS = onNixOS; devBuild = devBuild; profileBuild = profileBuild; pkgs = pkgs; }
+pkgs.callPackage ./Simula.nix { onNixOS = onNixOS; devBuild = devBuild; pkgs = pkgs; }
