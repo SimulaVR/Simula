50be6a8c759d5f5f715d199821b8aad6779b2e5b
haruki7049 <tontonkirikiri@gmail.com>
2024-11-20 20:50:25 +0900

    Delete pkgs definition in default.nix

--- default.nix
+++ default.nix
@@ -1,5 +1,8 @@
-{ onNixOS, devBuild, profileBuild ? false }:
-let
-    pkgs = if profileBuild then (import ./pinned-nixpkgs.nix { overlays = (import ./nix/profileOverlays.nix); }) else (import ./pinned-nixpkgs.nix { });
-in
-pkgs.callPackage ./Simula.nix { onNixOS = onNixOS; devBuild = devBuild; profileBuild = profileBuild; pkgs = pkgs; }
+{
+  callPackage,
+  onNixOS,
+  devBuild,
+  profileBuild ? false,
+}:
+
+callPackage ./Simula.nix { inherit onNixOS devBuild profileBuild; }
