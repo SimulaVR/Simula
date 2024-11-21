b071cff675576faecab3e6669229604e13ad559d
haruki7049 <tontonkirikiri@gmail.com>
2024-11-21 23:01:08 +0900

    Enable allowUnfree flag for nixpkgs

--- flake.nix
+++ flake.nix
@@ -28,6 +28,7 @@
         {
           _module.args.pkgs = import inputs.nixpkgs {
             inherit system;
+            config.allowUnfree = true;
             overlays = [ inputs.nixgl.overlays.default ];
           };
 
