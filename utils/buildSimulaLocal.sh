8553f120ae2e2d806f232c3e1a775ea11a4ae945
haruki7049 <tontonkirikiri@gmail.com>
2025-01-06 10:33:44 +0900

    Wrap 'cabal update' with nix develop

--- utils/buildSimulaLocal.sh
+++ utils/buildSimulaLocal.sh
@@ -4,12 +4,14 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-nix build .?submodules=1#devBuild-onNixOS # installSimula
-cabal update
-nix run .?submodules=1#build-monado
-nix run .?submodules=1#build-wlroots
-nix run .?submodules=1#build-godot -- build
-nix run .?submodules=1#patch-godot-wlroots
-nix run .?submodules=1#build-godot-haskell
-nix run .?submodules=1#build-godot-haskell-plugin
-nix run .?submodules=1#switch-to-local
+nix develop .?submodules=1#default --command sh -c '
+    nix build .?submodules=1#devBuild-onNixOS # installSimula
+    cabal update
+    nix run .?submodules=1#build-monado
+    nix run .?submodules=1#build-wlroots
+    nix run .?submodules=1#build-godot -- build
+    nix run .?submodules=1#patch-godot-wlroots
+    nix run .?submodules=1#build-godot-haskell
+    nix run .?submodules=1#build-godot-haskell-plugin -- build
+    nix run .?submodules=1#switch-to-local
+    '
