693c08d88bf4c598a68031d3809266d94393d5e6
George Singer <george.w.singer@gmail.com>
2026-02-05 15:59:24 -0600

    Fix patchelf bug in Helpers.sh

--- utils/Helpers.sh
+++ utils/Helpers.sh
@@ -16,11 +16,11 @@ updateEmail() {
 # wlroots in the nix store
 patchGodotWlroots(){
     PATH_TO_SIMULA_WLROOTS="`pwd`/submodules/wlroots/build/"
-    OLD_RPATH="`./result/bin/patchelf --print-rpath submodules/godot/bin/godot.x11.tools.64`"
+    OLD_RPATH="`patchelf --print-rpath submodules/godot/bin/godot.x11.tools.64`"
     if [[ $OLD_RPATH != $PATH_TO_SIMULA_WLROOTS* ]]; then # Check if the current RPATH contains our local simula wlroots build. If not, patchelf it to add it
         echo "Patching godot.x11.tools to point to local wlroots lib"
         echo "Changing path to: $PATH_TO_SIMULA_WLROOTS:$OLD_RPATH"
-        ./result/bin/patchelf --set-rpath "$PATH_TO_SIMULA_WLROOTS:$OLD_RPATH" submodules/godot/bin/godot.x11.tools.64
+        patchelf --set-rpath "$PATH_TO_SIMULA_WLROOTS:$OLD_RPATH" submodules/godot/bin/godot.x11.tools.64
     else
         echo "Not patching godot.x11.tools, already patched."
     fi
